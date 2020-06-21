99ef63970b272683dc1b4081614503bcd3298272
MuvvalaKaran <karanmuvvala@yahoo.in>
2020-06-21 14:30:31 -0600

    attempting problem 2 of assignment 1. Struggling with parsing inputs.

--- Assignments/Assignment 1/main.cpp
+++ Assignments/Assignment 1/main.cpp
@@ -5,6 +5,13 @@
 # include <string>
 
 using namespace std;
+// problem 2
+struct Park {
+                string parkname;
+                string state;
+                int area;
+            };
+
 // problem 1 
 
 int insertIntoSortedArray(int *myArray, int numEntries, int newValue){
@@ -49,10 +56,41 @@ int insertIntoSortedArray(int *myArray, int numEntries, int newValue){
 
 }
 
+void addPark(Park parks[], string parkname, string state, int area, int length){
+    // no. of items currently stored in the array
+    Park p;
+    p.parkname = parkname;
+    p.state = state;
+    p.area = area;
+    parks[length] = p;
+}
+
+
+void printList(const Park parks[], int length){
+    // length is the numbers of elements in the array 
+    for (int i=0; i< length; i++)
+    {
+        cout << parks[i].parkname << " [" << 
+        parks[i].state << "] area: "<< parks[i].area << endl;
+    }
+}
+
 // argc is the total # of arguments
 // argv is an array storing all the arguments passed
 int main(int argc, char const *argv[]){
+    // get user inputs
+    string ip_file_name = argv[1];
+    string op_file_name = argv[2];
 
+    string min_area = argv[3];
+    long int il_min_area = stol(min_area);
+
+    // create parks array
+    Park p_array[100];
+    
+    int case_num;
+    cout << "Enter problem no: \n";
+    cin >> case_num;
     // a hardcoded value of # of ints to expect
     int num_of_ints = 100; 
     // tmp variable to store a variable
@@ -61,29 +99,90 @@ int main(int argc, char const *argv[]){
     int array[num_of_ints] = {0};
     int num_of_arr_ele = 0;
     // create a handle to read the file
-    ifstream iFile("/home/karan-m/Documents/learning_hub/learn c++/summer_20_CSCI_2270/Assignments/Assignment\ 1/numbers.txt");
+    ifstream iFile("/home/karan-m/Documents/learning_hub/learn c++/summer_20_CSCI_2270/Assignments/Assignment 1/numbers.txt");
 
-    if (iFile.fail()){
-        cout << "Failed to open the file. \n";
-        return 0;
-    }
-    while (getline(iFile, s_num)){
-        i_num = stoi(s_num);
-        num_of_arr_ele++;
-        num_of_arr_ele = insertIntoSortedArray(array,num_of_arr_ele , i_num);
-    }
+    switch (case_num){
+        case 1:
+            // do operations related to problem 1
 
-    // print the array
-    for (int k = 0; k<num_of_arr_ele; k++){
-        cout << array[k] << endl;
-    }
+            if (iFile.fail()){
+                cout << "Failed to open the file. \n";
+                return 0;
+            }
+            while (getline(iFile, s_num)){
+                i_num = stoi(s_num);
+                num_of_arr_ele++;
+                num_of_arr_ele = insertIntoSortedArray(array,num_of_arr_ele , i_num);
+            }
 
-    if(num_of_arr_ele == num_of_ints)
-    {
-        // once done looping over all the elements dump the output to a txt file
-        ofstream oFile ("sorted_array_output.txt");
-        for (int k = 0; k < num_of_ints; k++){
-            oFile << array[k] << endl;
-        } 
+            // print the array
+            for (int k = 0; k<num_of_arr_ele; k++){
+                cout << array[k] << endl;
+            }
+
+            if(num_of_arr_ele == num_of_ints)
+            {
+                // once done looping over all the elements dump the output to a txt file
+                ofstream oFile ("sorted_array_output.txt");
+                for (int k = 0; k < num_of_ints; k++){
+                    oFile << array[k] << endl;
+                } 
+            }
+
+            return 0;
+        case 2:
+           
+            ifstream file_handle(ip_file_name);
+            string tmp_S;
+            string p_name = "";
+            string p_state ="";
+            string p_area = "";
+            
+            
+            // stringstream ss;
+            // variable to keep track of the number of lines
+            int iternum = 0;
+            int idx = 0;
+            // parse through each line
+            while (!file_handle.eof()){
+                int i;
+                iternum++;
+                getline(file_handle, tmp_S);
+                for (i=0; i<tmp_S.length(); i++){
+                    if (tmp_S[i] == ','){
+                        for (int j = 0; j < i; j++){
+                            p_name = p_name + tmp_S[j];
+                        }
+                    }
+                    cout << p_name;
+                    i++;
+                    for (int j = i; j < i+2; j++){
+                        p_state = p_state + tmp_S[j];
+                    }
+                    cout << p_state;
+                    i++;
+                    for (int j = i+2; j < tmp_S.length(); j++){
+                        p_area = p_area + tmp_S[j];
+                    }
+                    i = tmp_S.length();
+                    cout << p_area;
+                }
+
+                if (p_area != ""){
+                    // addPark(p_array, p_name, p_state, stoi(p_area), iternum);
+                }
+                p_name = "";
+                p_state = "";
+                p_area = "";
+            }
+
+            printList(p_array, iternum);
+
+
+            return 0;
+            
     }
+        
+
+   
 }
---
+++ Assignments/Assignment 1/test.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a[] = {1,2,3};
+
+    // cout << a + 2;
+    // cout << *(a + 10);
+    // cout << *a;
+    cout << *a[0];
+}
