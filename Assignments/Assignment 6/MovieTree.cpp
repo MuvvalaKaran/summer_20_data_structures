13923bac01408ec29f23bef054d00fa1fd894d19
MuvvalaKaran <karanmuvvala@yahoo.in>
2020-07-10 00:32:46 -0600

    added implementation for the add nodes into the tree and printing them. Quering function does not work yet. WIP: need to fix query implementation and check averaging function.

--- Assignments/Assignment 6/MovieTree.cpp
+++ Assignments/Assignment 6/MovieTree.cpp
@@ -1,9 +1,17 @@
 # include "MovieTree.hpp"
+# include <iostream>
 
 using namespace std;
 
+void inOrder(MovieNode* node);
+bool findMovieTraversal(MovieNode* root, string title);
+float averageTraversal(MovieNode* root, float sum, int count);
+void queryMoviesTraversal(MovieNode* root, float rating , int year);
+void addNodeTraversal(MovieNode* root,MovieNode* node);
+
 MovieTree::MovieTree(){
     // Intialize any member varibales of the class to defau;t
+    root = nullptr;
 }
 
 MovieTree::~MovieTree(){
@@ -12,22 +20,130 @@ MovieTree::~MovieTree(){
 
 void MovieTree::printMovieInventory(){
     // print every node in the tree in alphabetical order of titles
+    if (root == nullptr){
+        cout << "Tree is Empty, Cannot print" << endl;
+    }
+    else{
+        // we have to perform inorder traversal as we print Left sub tree first, the currrent root 
+        // and then the right sub-tree
+        inOrder(root);
+    }
 }
 
 void MovieTree::addMovieNode(int ranking, string title, int year, float rating){
     // A function to add a node IN the correct place based on its title. 
     // NOTE: No two movies have the same title
+    MovieNode *node = new MovieNode(ranking, title, year, rating);
+
+    // compare the this node with the root and go to left is < and right if > 
+    if (root == nullptr){
+        root = node;
+    }else{
+        addNodeTraversal(root, node);
+    }
+    
+    
 }
 
 void MovieTree::findMovie(string title){
     // A function to find the movie with the given title and print its corresponding information
+    // bool found = findMovieTraversal(root, title); 
+    if (!findMovieTraversal(root, title)){
+        cout << "Movie not found." << endl;
+    }
 }
 
 void MovieTree::queryMovies(float rating, int year){
     // A function to print all the moview with a rating of at least @rating and that were released before the year 
     // @year in the PREORDER traversal fashion.
+    cout << "Movies that came out after " << year << " with rating at least " << rating 
+    << ":" << endl;
+    queryMoviesTraversal(root, rating, year);
 }
 
 void MovieTree::averageRating(){
     // A function to print the average rating for all movies in the tree. 
+    float average = averageTraversal(root, 0, 0);
+    cout << "Average rating:" << average << endl;
+}
+
+void inOrder(MovieNode* node){
+
+    if (node != nullptr){
+        inOrder(node->left);
+        cout << "Movie: " << node->title << " " << node->rating << endl;
+        inOrder(node->right);
+    }
+}
+
+void addNodeTraversal(MovieNode* root, MovieNode* node){
+    if (root != nullptr){
+        if (node->title <= root->title){
+            if (root->left != nullptr){
+                addNodeTraversal(root->left, node);
+            }
+            else{
+                root->left = node;
+            }
+            
+        }
+        else if (node->title[0] >= root->title[0])
+        {
+            if (root->right != nullptr){
+                addNodeTraversal(root->right, node);
+            }
+            else{
+                root->right = node;
+            }
+        }
+    }
+}
+
+bool findMovieTraversal(MovieNode* root, string title){
+    if (root != nullptr){
+        if (root->title != title){
+            if (root->left){
+                findMovieTraversal(root->left, title);
+            }
+            if (root->right){
+                findMovieTraversal(root->right, title);
+            }
+        }
+        else{
+            cout << "Movie Info:" << endl;
+            cout << "=====================" << endl;
+            cout << "Ranking:"  << root->ranking << endl;
+            cout << "Title:"  << root->title << endl;
+            cout << "Year:"  << root->year << endl;
+            cout << "rating:"  << root->rating << endl;
+            return true;
+        }
+    } 
+    // return false;
+}
+
+void queryMoviesTraversal(MovieNode* root, float rating , int year){
+    if (root != nullptr){
+        if ((root->rating >= rating) && (root->year > year)){
+            cout << root->title << "(" << root->year << ")" << root->rating << endl;
+        }
+    }
+    else{
+        cout << "Tree is Empty. Cannot query Movies" << endl;
+    }
+}
+
+float averageTraversal(MovieNode* root, float sum, int count){
+
+    if (root == nullptr){
+        return 0.0;
+    }
+    else{
+        sum += root->rating;
+        count++;
+        averageTraversal(root->left, sum, count);
+        averageTraversal(root->right, sum , count);    
+    }
+
+    return sum/count;
 }
--- Assignments/Assignment 6/MovieTreeDriver.cpp
+++ Assignments/Assignment 6/MovieTreeDriver.cpp
@@ -1,11 +1,54 @@
 # include "MovieTree.cpp"
 # include <iostream>
+# include <fstream> 
+# define debug true
 
 using namespace std;
 
 void menu();
 
+void addData(MovieTree tree, string file_name);
+
 int main(int argc, char* argv[]){
+    // get user input
+    string ip_file_name;
+    if (debug){
+        ip_file_name = "Movies.csv";
+    }
+    else{
+        ip_file_name = argv[1];
+    }
+    MovieTree tree;
+
+    // addData(tree, ip_file_name);
+    //open the file
+    ifstream iFile("/home/karan-m/Documents/learning_hub/learn c++/summer_20_CSCI_2270/Assignments/Assignment 6/" + ip_file_name);
+
+    if (iFile.fail()){
+        cout << "Error opening the file. Please make sure you enter the right name" << endl;
+        return 0;
+    }
+
+    string ranking;
+    string rating;
+    string year;
+    string title;
+    int iranking;
+    float frating;
+    int iyear;
+    // the ip format is <ranking>,<title>,<year>,<rating>
+    while (getline(iFile, ranking, ',')){
+        getline(iFile, title, ',');
+        getline(iFile, year, ',');
+        getline(iFile, rating);
+
+        // convert to approproate formats
+        iyear = stoi(year);
+        frating = stof(rating);
+        iranking = stoi(ranking);
+
+        tree.addMovieNode(iranking, title, iyear, frating);
+    }
 
     int choice;
     while (1){
@@ -15,18 +58,32 @@ int main(int argc, char* argv[]){
         switch (choice){
 
             case 1:{
+                string movie;
+                cout << "Enter title:" << endl;
+                cin.ignore();
+                getline(cin, movie);
+                tree.findMovie(movie);
                 break;
             }
 
             case 2:{
+                float r;
+                int y;
+                cout << "Enter minimum rating:" << endl;
+                cin >> r;
+                cout << "Enter minimum year:" << endl;
+                cin >> y;
+                tree.queryMovies(r, y);
                 break;
             }
 
             case 3:{
+                tree.printMovieInventory();
                 break;
             }
 
             case 4:{
+                tree.averageRating();
                 break;
             }
 
@@ -45,4 +102,35 @@ void menu(){
     cout << "3. Print the inventory" << endl;
     cout << "4. Average Rating of movies" << endl;
     cout << "5. Quit" << endl;
+}
+
+void addData(MovieTree tree, string file_name){
+    //open the file
+    ifstream iFile("/home/karan-m/Documents/learning_hub/learn c++/summer_20_CSCI_2270/Assignments/Assignment 6/" + file_name);
+
+    if (iFile.fail()){
+        cout << "Error opening the file. Please make sure you enter the right name" << endl;
+        return;
+    }
+
+    string ranking;
+    string rating;
+    string year;
+    string title;
+    int iranking;
+    float frating;
+    int iyear;
+    // the ip format is <ranking>,<title>,<year>,<rating>
+    while (getline(iFile, ranking, ',')){
+        getline(iFile, title, ',');
+        getline(iFile, year, ',');
+        getline(iFile, rating);
+
+        // convert to approproate formats
+        iyear = stoi(year);
+        frating = stof(rating);
+        iranking = stoi(ranking);
+
+        tree.addMovieNode(iranking, title, iyear, frating);
+    }
 }
